1bfb1730dfcc36977c80a81c51e5031c46922806
paupc <p.pulido.c@institutvidreres.cat>
2024-12-19 09:33:32 +0100

    HomeActivity binding

--- app/src/main/java/com/paudam/projectecafeteriapaupulido/ActivityHome.kt
+++ app/src/main/java/com/paudam/projectecafeteriapaupulido/ActivityHome.kt
@@ -5,16 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.paudam.projectecafeteriapaupulido.databinding.ActivityHomeBinding
 
 class ActivityHome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding: ActivityHomeBinding = DataBindingUtil.setContentView(this, R.layout.activity_home)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
     }
 }
