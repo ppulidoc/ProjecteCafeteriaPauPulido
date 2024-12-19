4e98a2a66a1e62303905a6dbd1f56ee235915304
paupc <p.pulido.c@institutvidreres.cat>
2024-12-19 09:31:15 +0100

    Login Page funciona, user y passwd done y cambia a homeActivity

--- app/src/main/java/com/paudam/projectecafeteriapaupulido/ActivityLogin.kt
+++ app/src/main/java/com/paudam/projectecafeteriapaupulido/ActivityLogin.kt
@@ -1,20 +1,37 @@
 package com.paudam.projectecafeteriapaupulido
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.paudam.projectecafeteriapaupulido.databinding.ActivityLoginBinding
 
 class ActivityLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding: ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding.buttonLogin.setOnClickListener {
+            val user = binding.userText.text.toString()
+            val passwd = binding.PasswordText.text.toString()
+            if ( user == "ppulido" && passwd == "123456"){
+                val ActividadHome = Intent(this, ActivityHome::class.java)
+                startActivity(ActividadHome)
+            }
+            else {
+                val ErrLoginMssg = "USER O CONTRA INCORRECTO"
+                val duration = Toast.LENGTH_SHORT
+                val toast = Toast.makeText(this, ErrLoginMssg, duration)
+                toast.show()
+            }
+
+
         }
+
     }
 }
