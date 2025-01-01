a49bdb7d3e1cc13fe9ac8962ae3535146b0de88f
pau <pau.pulido@institutvidreres.cat>
2025-01-01 22:39:59 +0100

    Navegacion botones hecha

--- app/src/main/java/com/paudam/projectecafeteriapaupulido/BegudesFragment.kt
+++ app/src/main/java/com/paudam/projectecafeteriapaupulido/BegudesFragment.kt
@@ -5,15 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import com.paudam.projectecafeteriapaupulido.databinding.FragmentBegudesBinding
+import com.paudam.projectecafeteriapaupulido.databinding.FragmentMenjarsBinding
 
 class BegudesFragment : Fragment() {
+
+    private lateinit var binding: FragmentBegudesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_begudes, container, false)
+        binding = FragmentBegudesBinding.inflate(inflater)
+
+        binding.button2.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_begudesFragment_to_postresFragment,
+                null)
+        }
+
+
+        return binding.root
     }
 
 }
--- app/src/main/java/com/paudam/projectecafeteriapaupulido/MenjarsFragment.kt
+++ app/src/main/java/com/paudam/projectecafeteriapaupulido/MenjarsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
 import com.paudam.projectecafeteriapaupulido.databinding.FragmentMenjarsBinding
 
 
@@ -18,6 +20,16 @@ class MenjarsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentMenjarsBinding.inflate(inflater)
+
+        binding.button2.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_menjarsFragment_to_begudesFragment,
+                null,
+                NavOptions.Builder()
+                    .setPopUpTo(R.id.menjarsFragment, true)
+                    .build()
+            )
+        }
 
         return binding.root
 
