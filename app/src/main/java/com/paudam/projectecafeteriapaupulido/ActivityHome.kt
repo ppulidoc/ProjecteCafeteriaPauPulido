132efdfc363084a56f07872429a67299781bff12
pau <pau.pulido@institutvidreres.cat>
2025-01-01 18:54:07 +0100

    bottom navigation menu hecho

--- app/src/main/java/com/paudam/projectecafeteriapaupulido/ActivityHome.kt
+++ app/src/main/java/com/paudam/projectecafeteriapaupulido/ActivityHome.kt
@@ -6,14 +6,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.paudam.projectecafeteriapaupulido.databinding.ActivityHomeBinding
 
 class ActivityHome : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityHomeBinding = DataBindingUtil.setContentView(this, R.layout.activity_home)
         enableEdgeToEdge()
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupWithNavController(binding.bottomNavigationView, navController)
 
     }
 }
