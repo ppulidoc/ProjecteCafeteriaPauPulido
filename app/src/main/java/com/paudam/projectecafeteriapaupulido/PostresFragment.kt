991d53ca9262a96910b0048fa9879b4302d7832d
pau <pau.pulido@institutvidreres.cat>
2025-01-02 16:39:41 +0100

    ViewModel postres feta (Obtinc tots els valors i el total )

--- app/src/main/java/com/paudam/projectecafeteriapaupulido/PostresFragment.kt
+++ app/src/main/java/com/paudam/projectecafeteriapaupulido/PostresFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.paudam.projectecafeteriapaupulido.databinding.FragmentBegudesBinding
@@ -13,7 +15,7 @@ import com.paudam.projectecafeteriapaupulido.databinding.FragmentPostresBinding
 
 
 class PostresFragment : Fragment() {
-
+    private val postresViewModel: PostresViewModel by activityViewModels()
     private lateinit var binding: FragmentPostresBinding
 
     override fun onCreateView(
@@ -24,6 +26,35 @@ class PostresFragment : Fragment() {
         binding = FragmentPostresBinding.inflate(inflater)
 
         binding.button2.setOnClickListener {
+
+            val quantitatT = binding.quantTrufas.text.toString()
+            val preuT = binding.preuTrufa.text.toString()
+            val quantTInt = quantitatT.toIntOrNull()
+            val preuTInt = preuT.toIntOrNull()
+
+            if (quantTInt != null && preuTInt != null ) {
+                postresViewModel.updateTrufes(quantTInt, preuTInt)
+            }
+
+
+            val quantitatF = binding.quantFlam.text.toString()
+            val preuF = binding.preuFlam.text.toString()
+            val quantFInt = quantitatF.toIntOrNull()
+            val preuFInt = preuF.toIntOrNull()
+
+            if (quantFInt != null && preuFInt != null ) {
+                postresViewModel.updateFlam(quantFInt, preuFInt)
+            }
+
+
+            postresViewModel.totalPostres()
+
+            val mensaje = postresViewModel.postres.value?.toString() ?: "No hay valor"
+            val duracion = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(requireContext(), mensaje, duracion)
+            toast.show()
+
+
             findNavController().navigate(
                 R.id.action_postresFragment_to_pagamentFragment,
                 null)
--- app/src/main/java/com/paudam/projectecafeteriapaupulido/PostresViewModel.kt
+++ app/src/main/java/com/paudam/projectecafeteriapaupulido/PostresViewModel.kt
@@ -1,4 +1,60 @@
-package com.paudam.projectecafeteriapaupulido;
+package com.paudam.projectecafeteriapaupulido
 
-public class PostresViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+class PostresViewModel: ViewModel() {
+
+    private val _postres = MutableLiveData<Int>()
+    val postres: LiveData<Int> get() = _postres
+
+    //Trufes
+    private val _trufes = MutableLiveData<Int>()
+    val trufes: LiveData<Int> get() = _trufes
+
+    private val _trufesQ = MutableLiveData<Int>()
+    val trufesQ: LiveData<Int> get() = _trufesQ
+
+    private val _trufesP = MutableLiveData<Int>()
+    val trufesP: LiveData<Int> get() = _trufesP
+
+    private val _trufesT = MutableLiveData<Int>()
+    val trufesT: LiveData<Int> get() = _trufesT
+
+    fun updateTrufes(quant: Int, preu: Int) {
+        _trufesQ.value = quant
+        _trufesP.value = preu
+        val totalTrufes = (_trufesQ.value ?: 0) * (_trufesP.value ?: 0)
+        _trufesT.value = totalTrufes
+    }
+
+    //Flam
+
+    private val _flam = MutableLiveData<Int>()
+    val flam: LiveData<Int> get() = _flam
+
+    private val _flamQ = MutableLiveData<Int>()
+    val flamQ: LiveData<Int> get() = _flamQ
+
+    private val _flamP = MutableLiveData<Int>()
+    val flamP: LiveData<Int> get() = _flamP
+
+    private val _flamT = MutableLiveData<Int>()
+    val flamT: LiveData<Int> get() = _flamT
+
+    fun updateFlam(quant: Int, preu: Int) {
+        _flamQ.value = quant
+        _flamP.value = preu
+        val totalFlams = (_flamQ.value ?: 0) * (_flamP.value ?: 0)
+        _flamT.value = totalFlams
+    }
+
+    fun totalPostres() {
+        val sumaTotal = (_trufesT.value ?: 0) + (flamT.value ?: 0)
+        _postres.value = sumaTotal
+
+    }
+
 }
